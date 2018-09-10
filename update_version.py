b2412f9f73fcdddb6727592461675eed47c7eb16
Kamil Galladjov <gallkam@outlook.com>
2018-09-10 21:29:13 +0300

    version updater modificated

--- update_version.py
+++ update_version.py
@@ -1,32 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
+import json
 
 if __name__ == '__main__':
-    version = sys.argv[1]
-    setup = (
-       "#!/usr/bin/env python\n"
-       "# -*- coding: utf-8 -*-\n"
-       "\n"
-       "from setuptools import setup, find_packages\n"
-       "\n"
-       "with open('README.md', 'r') as fh:\n"
-       "    long_description = fh.read()\n"
-       "    setup(\n"
-       "        name='msu_helpers',\n"
-       "        packages=find_packages(),\n"
-       "        description='Package created for the university project to store common "
-       "applications logic in one place. I am pretty sure that you do not need it',\n"
-       "        long_description=long_description,\n"
-       "        long_description_content_type='text/markdown',\n"
-       f"        version='{version}',\n"
-       "        url='https://github.com/Ujinjinjin/msu_helpers',\n"
-       "        author='ujinjinjin',\n"
-       "        author_email='gallkam@outlook.com ',\n"
-       "        keywords=['pip','msu_sqluniversity'],\n"
-       "    )\n"
-    )
 
-    with open('setup.py', 'w') as fw:
-        fw.write(setup)
+    with open('version.json', 'r', encoding='utf-8') as f:
+        version_dict = json.load(f)
+
+    version_dict['build'] += 1
+
+    with open('version.json', 'w', encoding='utf-8') as f:
+        json.dump(version_dict, f)
