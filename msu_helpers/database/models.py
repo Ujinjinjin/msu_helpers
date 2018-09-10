ab42a4ea2bad8774c507ffef777ad6eb4efb7b57
Kamil Galladjov <gallkam@outlook.com>
2018-09-10 20:28:00 +0300

    Divided user model into extended and base
    Extended can be used when create, delete and update methods required. Base supposed to be used only for retrieve operations

--- msu_helpers/database/models.py
+++ msu_helpers/database/models.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import django
 from django.contrib import admin
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from .db_constants import *
-from .managers import UserManager
-
-django.setup()
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(models.Model):
     first_name = models.CharField(max_length=64, default=UserDefaults.first_name)
     last_name = models.CharField(max_length=64, default=UserDefaults.last_name)
     study_group = models.CharField(max_length=10, default=UserDefaults.study_group)
@@ -26,11 +20,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     lang = models.CharField(max_length=5, default=UserDefaults.lang)
     activated = models.BooleanField(default=UserDefaults.activated)
     is_staff = models.BooleanField(default=UserDefaults.is_staff, editable=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _('User')
@@ -67,36 +56,6 @@ class User(AbstractBaseUser, PermissionsMixin):
             'activated': self.activated,
             'is_staff': self.is_staff,
         }
-
-    @classmethod
-    def deserialize(cls, data: dict, save: bool = False):
-        user = User()
-
-        user.pk = data.get('id', UserDefaults.id)
-        user.first_name = data.get('first_name', UserDefaults.first_name)
-        user.last_name = data.get('last_name', UserDefaults.last_name)
-        user.study_group = data.get('study_group', UserDefaults.study_group)
-        user.about = data.get('about', UserDefaults.about)
-        user.profile_pic = data.get('profile_pic', UserDefaults.profile_pic)
-        user.email = data.get('email', UserDefaults.email)
-        user.lang = data.get('lang', UserDefaults.lang)
-        user.activated = data.get('activated', UserDefaults.activated)
-        user.is_staff = data.get('is_staff', UserDefaults.is_staff)
-
-        birthday = data.get('birthday', UserDefaults.birthday)
-        password = data.get('password', None)
-
-        if password is None or user.email is None:
-            raise ValueError('Can not create user without email or password')
-
-        if birthday is not None:
-            user.birthday = datetime.strptime(birthday, Utils.DATETIME_FORMAT)
-
-        user.set_password(password)
-        if save:
-            user.save()
-
-        return user
 
 
 class UserAdmin(admin.ModelAdmin):
