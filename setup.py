ef78a8c4c031d3115b73a413fbb8c377a9adf671
Kamil Galladjov <gallkam@outlook.com>
2018-09-10 22:10:37 +0300

    Final version updater

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import json
-
-with open('version.json', 'r', encoding='utf-8') as f:
-    version_dict = json.load(f)
-
-pkg_version = f'{version_dict["major"]}.{version_dict["minor"]}.{version_dict["build"]}'
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
@@ -18,7 +12,7 @@ setup(
     description='Package created for the university project to store common applications logic in one place. I am pretty sure that you do not need it',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version=pkg_version,
+    version='',
     url='https://github.com/Ujinjinjin/msu_helpers',
     author='ujinjinjin',
     author_email='gallkam@outlook.com ',
--- update_version.py
+++ update_version.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
 import sys
 
 if __name__ == '__main__':
+    version: str = sys.argv[1]
+    version = version.split('.')
+    build = int(version[1]) + 1
 
-    print(sys.argv)
-    
-    with open('version.json', 'r', encoding='utf-8') as f:
-        version_dict = json.load(f)
+    version = f'0.3.{build}'
 
-    version_dict['build'] += 1
+    setup = (
+        "#!/usr/bin/env python\n"
+        "# -*- coding: utf-8 -*-\n"
+        "\n"
+        "from setuptools import setup, find_packages\n"
+        "\n"
+        "with open('README.md', 'r') as fh:\n"
+        "    long_description = fh.read()\n"
+        "setup(\n"
+        "    name='msu_helpers',\n"
+        "    packages=find_packages(),\n"
+        "    description='Package created for the university project to store common "
+        "applications logic in one place. I am pretty sure that you do not need it',\n"
+        "    long_description=long_description,\n"
+        "    long_description_content_type='text/markdown',\n"
+        f"    version='{version}',\n"
+        "    url='https://github.com/Ujinjinjin/msu_helpers',\n"
+        "    author='ujinjinjin',\n"
+        "    author_email='gallkam@outlook.com ',\n"
+        "    keywords=['pip','msu_sqluniversity'],\n"
+        ")\n"
+    )
 
-    with open('version.json', 'w', encoding='utf-8') as f:
-        json.dump(version_dict, f)
+    with open('setup.py', 'w') as fw:
+        fw.write(setup)
