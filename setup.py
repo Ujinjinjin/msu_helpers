a672fe0d9a314bbf96a955e2624511bf6fed3918
Kamil Galladjov <gallkam@outlook.com>
2018-09-10 14:25:34 +0300

    Initial lib commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='msu_helpers',
+    packages=['msu_helpers'],
+    description='Package created for the university project to store common applications logic in one place. I am pretty sure that you do not need it',
+    version='0.1',
+    url='',  # TODO Paste here git link
+    author='Ujinjinjin',
+    author_email='ujinjinjin@outlook.com ',
+    keywords=['pip','msu_sqluniversity']
+    )
