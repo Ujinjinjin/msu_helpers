177755d9270ed1e350dcb49ef4d7aa95164fd072
Kamil <gallkam@outlook.com>
2018-10-06 18:47:50 +0300

    UserSearchModel: fixed crash on empty filters request

--- msu_helpers/interface/models/user.py
+++ msu_helpers/interface/models/user.py
@@ -5,13 +5,28 @@ __all__ = ('UserSearchModel',)
 
 class UserSearchModel:
     def __init__(self, value=''):
-        self.parsed_data = re.match('(?P<first_name>[\wА-Яа-я]+)'
-                                    '(/(?P<last_name>[\wА-Яа-я]+))?'
-                                    '(/(?P<group_code>[\wА-Яа-я\-\d]{0,10}))?',
-                                    value).groupdict()
+        param_match = re.match('(?P<first_name>[\wА-Яа-я]+)'
+                               '(/(?P<last_name>[\wА-Яа-я]+))?'
+                               '(/(?P<group_code>[\wА-Яа-я\-\d]{0,10}))?',
+                               value)
+
+        if param_match:
+            self.parsed_data = param_match.groupdict()
+        else:
+            self.parsed_data = dict()
+
         self.first_name: str = self.parsed_data.get('first_name', '')
         self.last_name: str = self.parsed_data.get('last_name', '')
         self.group_code: str = self.parsed_data.get('group_code', '')
+
+        if self.first_name is None:
+            self.first_name = ''
+
+        if self.last_name is None:
+            self.last_name = ''
+
+        if self.group_code is None:
+            self.group_code = ''
 
     def __str__(self):
         return f'{self.parsed_data}'
