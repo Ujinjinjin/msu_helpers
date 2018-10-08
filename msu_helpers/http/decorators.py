b20d7d144a36e05979f8eb993da813037cd605b7
Kamil <gallkam@outlook.com>
2018-10-08 18:26:09 +0300

    forgot to add from_query decorator to __all__

--- msu_helpers/http/decorators.py
+++ msu_helpers/http/decorators.py
@@ -1,6 +1,6 @@
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
 
-__all__ = ('get', 'post', 'http_method')
+__all__ = ('get', 'post', 'http_method', 'from_query')
 
 http_method = require_http_methods
 get = require_GET
